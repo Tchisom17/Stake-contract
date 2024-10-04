35f2add93847933e46ef15726adee7aa147941b9
Tchisom <Kamsiyochukwu1@gmail.com>
2024-10-04 15:57:52 +0100

    feat: Add StakeEther deployment module using Hardhat ignition

---
+++ ignition/modules/StakeEther.ts
@@ -0,0 +1,9 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const StakeEtherModule = buildModule("StakeEtherModule", (m) => {
+  const stakeEther = m.contract("StakeEther");
+
+  return { stakeEther };
+});
+
+export default StakeEtherModule;
